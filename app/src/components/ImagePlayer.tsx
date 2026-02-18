44dd926dd9446e406b5c74c6b2b4ec3fcb2dc7b9
HK-SHAO <hk-shao@outlook.com>
2026-02-18 17:31:05 +0800

    update

--- app/src/components/ImagePlayer.tsx
+++ app/src/components/ImagePlayer.tsx
@@ -612,9 +612,9 @@ export function ImagePlayer() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               className="w-[50%] aspect-square text-white"
             >
               <circle cx="12" cy="13" r="8" />
