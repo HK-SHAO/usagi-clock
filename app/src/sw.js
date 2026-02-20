4235d7c64b51c951d6f98c328b63e45c1101f32a
HK-SHAO <hk-shao@outlook.com>
2026-02-20 12:16:36 +0800

    add gtag

--- app/src/sw.js
+++ app/src/sw.js
@@ -1,20 +1,21 @@
-const CACHE_NAME = 'usagi-clock-cache-v1';
+const CACHE_NAME = "usagi-clock-cache-v1";
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/favicon.webp'
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./favicon.webp",
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches
+      .open(CACHE_NAME)
       .then((cache) => cache.addAll(ASSETS_TO_CACHE))
-      .then(() => self.skipWaiting())
+      .then(() => self.skipWaiting()),
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
@@ -22,31 +23,28 @@ self.addEventListener('activate', (event) => {
           if (cache !== CACHE_NAME) {
             return caches.delete(cache);
           }
-        })
+        }),
       );
-    })
+    }),
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then((res) => {
+        if (!res || res.status !== 200 || res.type !== "basic") {
+          return res;
         }
-        return fetch(event.request)
-          .then((res) => {
-            if (!res || res.status !== 200 || res.type !== 'basic') {
-              return res;
-            }
-            const responseToCache = res.clone();
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-            return res;
-          });
-      })
+        const responseToCache = res.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return res;
+      });
+    }),
   );
 });
